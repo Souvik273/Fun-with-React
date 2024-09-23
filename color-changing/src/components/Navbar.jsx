7fb27c5973258051a2cdbbbc5e24255f48504af1
Souvik273 <souvikgoswami464@gmail.com>
2024-09-23 11:38:08 +0530

    Optimising Button component using map method

--- color-changing/src/components/Navbar.jsx
+++ color-changing/src/components/Navbar.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
-import Button from './button'
+import React from "react";
+import Button from "./button";
 
-const Navbar = ({color,setColor}) => {
+const Navbar = ({ color, setColor }) => {
+  const colors = [
+    "Red",
+    "Green",
+    "Blue",
+    "Purple",
+    "Magenta",
+    "Lavender",
+    "Yellow",
+    "Pink",
+    "Black",
+  ];
   return (
-    <div className="w-full h-screen duration-200" style={{backgroundColor:color}}>
-
-      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
-        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-xl'>
-          <Button color="Red" setColor={setColor}/>
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-xl">
+          {/* <Button color="Red" setColor={setColor}/>
           <Button color="green" setColor={setColor}/>
           <Button color="blue" setColor={setColor}/>
           <Button color="purple" setColor={setColor}/>
@@ -15,12 +28,15 @@ const Navbar = ({color,setColor}) => {
           <Button color="lavender" setColor={setColor}/>
           <Button color="yellow" setColor={setColor}/>
           <Button color="pink" setColor={setColor}/>
-          <Button color="black" setColor={setColor}/>
+          <Button color="black" setColor={setColor}/> */}
+
+          {colors.map((clr) => (
+            <Button key={clr} color={clr} setColor={setColor} />
+          ))}
         </div>
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
